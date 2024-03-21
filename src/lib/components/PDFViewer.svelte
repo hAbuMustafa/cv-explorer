ca55e18f442155d5ab5000ca6da49ae39f2455ed
Hosam Sultan <hosam5553@gmail.com>
2024-03-21 13:03:38 +0000

    viewer component

---
+++ src/lib/components/PDFViewer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Viewer from 'svelte-pdf';
+
+  export let file: any;
+  export let tags: string[] = [];
+</script>
+
+<h1>{file}</h1>
+
+<ul>
+  {#each tags as tag (tag)}
+    <li>{tag}</li>
+  {/each}
+</ul>
+
+<Viewer url={file.path} data={''} />
+
+<style>
+</style>
