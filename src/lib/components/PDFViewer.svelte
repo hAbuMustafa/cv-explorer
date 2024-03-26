cd469b2a1e4de0ac61050819589edf5b1b025e10
Hosam Sultan <hosam5553@gmail.com>
2024-03-26 10:37:12 +0000

    use office 365 instead of google docs

--- src/lib/components/PDFViewer.svelte
+++ src/lib/components/PDFViewer.svelte
@@ -17,11 +17,23 @@
   {:else}
     <iframe
       title={`CV ${file.fileName}`}
+      src="https://view.officeapps.live.com/op/embed.aspx?src={$page.url.href +
+        file.path.replace('/src', 'src')}"
+      width="80%"
+      height="800px"
+      frameborder="1"
+      >This is an embedded <a target="_blank" href="http://office.com">Microsoft Office</a
+      >
+      document, powered by
+      <a target="_blank" href="http://office.com/webapps">Office Online</a>.</iframe
+    >
+    <!-- <iframe
+      title={`CV ${file.fileName}`}
       src="https://docs.google.com/gview?url={$page.url.href +
         file.path.replace('/src', 'src')}&embedded=true"
       style="height:800px; width:100%;"
       frameborder="0"
-    />
+    /> -->
   {/if}
 </div>
 
