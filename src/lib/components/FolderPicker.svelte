c8b21749c2a24bdbec2f9dd3d53b76c273be9b3a
Hosam <hosam5553@gmail.com>
2024-04-03 15:19:05 +0200

    created new browse component

---
+++ src/lib/components/FolderPicker.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let CVs: FileList;
+
+  function handleFileChange(e: Event) {
+    const files = (e.target as HTMLInputElement).files;
+    if (!files) {
+      alert(
+        "The specified folder doesn't contain any valid Resume files. Please, make sure that the folder contains PDFs and DOCXs."
+      );
+      return;
+    }
+    CVs = files;
+    console.log(CVs);
+  }
+</script>
+
+<div class="wrapper">
+  <h2>Please pick a folder to preview CVs</h2>
+  <input
+    type="file"
+    name="folder-picker"
+    id="folder-picker"
+    webkitdirectory
+    on:change={handleFileChange}
+  />
+</div>
+
+<style>
+</style>
