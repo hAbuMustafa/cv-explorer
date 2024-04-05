56e11c6b38a4254b4672a54f59f29ad16d30a604
hAbuMustafa <hosam5553@gmail.com>
2024-04-06 01:52:23 +0200

    correct implementation for pdfs

--- src/lib/components/PDFViewer.svelte
+++ src/lib/components/PDFViewer.svelte
@@ -1,49 +1,50 @@
 <script lang="ts">
-  export let file: File;
-  export let tags: string[] = [];
+  import mammoth from 'mammoth';
 
-  let reader = new FileReader();
-  reader.onload = function (e: ProgressEvent<FileReader>) {
-    let pdfData = e.target!.result;
-    let objectElem = document.createElement('object');
-    objectElem.data = pdfData;
-    document.body.appendChild(objectElem);
-  };
+  export let file: File;
+
+  async function fileToArrayBuffer(file: File): Promise<ArrayBuffer> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        resolve(reader.result as ArrayBuffer);
+      };
+
+      reader.onerror = reject;
+
+      reader.readAsArrayBuffer(file);
+    });
+  }
 </script>
 
-<ul>
-  {#each tags as tag (tag)}
-    <li>{tag}</li>
-  {/each}
-</ul>
-
 <div class="cv-wrapper">
-  {#if file.type.includes('pdf')}
-    <!-- <embed src={file.webkitRelativePath} width="80%" height="800px" type={file.type} /> -->
-    <object
-      data={reader.readAsDataURL(file)}
-      type={file.type}
-      aria-label={file.name}
+  {#if file.type === 'application/pdf'}
+    <iframe
+      src={URL.createObjectURL(file)}
+      title={file.name}
       width="80%"
       height="800px"
     />
   {:else}
-    <iframe srcdoc={file.stream()} title={file.name} width="80%" height="800px" />
+    <iframe
+      srcdoc={await mammoth.convertToHtml({
+        arrayBuffer: await fileToArrayBuffer(file),
+      })}
+      title={file.name}
+      width="80%"
+      height="800px"
+    />
   {/if}
 </div>
 
 <style>
-  li {
-    list-style: none;
-  }
-
   .cv-wrapper {
     text-align: center;
     position: relative;
   }
 
-  iframe,
-  embed {
+  iframe {
     border: 1px solid black;
   }
 </style>
